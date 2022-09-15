24776b883884163eac35deac0080695cd8a8bb03
dcevedo <37398700+dcevedo@users.noreply.github.com>
2022-09-15 12:21:18 -0500

    Add configuration file

---
+++ src/main/java/com/practices/inventory/configuration/GeneralConfiguration.java
@@ -0,0 +1,33 @@
+package com.practices.inventory.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GeneralConfiguration {
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource(){
+        return DataSourceBuilder.create()
+            .url(jdbcUrl)
+            .driverClassName(driverClassName)
+            .username(username)
+            .password(password)
+            .build();
+    }
+}
