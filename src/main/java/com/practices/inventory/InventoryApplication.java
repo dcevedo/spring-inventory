1538b8c16e3e3228efb2dae6e42c094ef38d7d58
dcevedo <37398700+dcevedo@users.noreply.github.com>
2022-09-19 23:33:03 -0500

    Add main class for docker without servlet

--- src/main/java/com/practices/inventory/InventoryApplication.java
+++ src/main/java/com/practices/inventory/InventoryApplication.java
@@ -2,21 +2,11 @@ package com.practices.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InventoryApplication extends SpringBootServletInitializer{
+public class InventoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
 	}
-/*
- * Implementation of spring app without the embeed tomcat server
- */
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(InventoryApplication.class);
-	}
-
 }
